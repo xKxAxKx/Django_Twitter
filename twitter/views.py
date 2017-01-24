cebfba311e3f1deed2d613bbaef9dbb1f4d119fc
xKxAxKx <pekepooooong@gmail.com>
2017-01-24 17:32:09 +0900

    edit twitter/index.html

--- twitter/views.py
+++ twitter/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from twitter.models import Tweet, Favorite
+from django.contrib.auth.models import User
+
 
 
 @login_required
 def index(request):
-    tweet = "tweetです"
+    tweets = Tweet.objects.select_related().all().order_by('-id')
     return render(request,
         'twitter/index.html',
-        {'tweet' : tweet}
+        {'tweets' : tweets}
     )
