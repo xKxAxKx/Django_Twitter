8aa7ceb2421f0a4b586a6d979070067b61aa8d17
xKxAxKx <pekepooooong@gmail.com>
2017-01-24 15:15:50 +0900

    migrate tweet

--- twitter/models.py
+++ twitter/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Tweet(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    reply_tweet_id = models.IntegerField(blank=True)
+    content = models.CharField(max_length=140)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+
+class Favorite(models.Model):
+    tweet_id = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
