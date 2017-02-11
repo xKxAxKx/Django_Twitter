01c51a5018c602978a3858644ca3f32bf9f9438a
xKxAxKx <pekepooooong@gmail.com>
2017-02-11 14:40:53 +0900

    signupにメールアドレスを追加

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,9 +5,14 @@ from django.contrib.auth.models import User
 
 
 class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
 
